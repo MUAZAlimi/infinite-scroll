1626e242c40dec40dadfe526a359064611d3ed88
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-22 11:46:57 +0100

    up

--- src/Post.js
+++ src/Post.js
@@ -1,23 +1,22 @@
-import React from 'react'
+/* eslint-disable no-undef */
+import React from "react";
 
-const Post = React.forwardRef(({post}) => {
-    const postBody = (
-        <>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-            <p>Post ID : {post.id}</p>
-        </>
-    )
+const Post = React.forwardRef(({ post }) => {
+  const postBody = (
+    <>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <p>Post ID : {post.id}</p>
+    </>
+  );
 
-    const content = ref ?
-                        <article ref={ref}>{postBodt}</article>
-                        :
-                        <article>{postBody}</article>
+  const content = ref ? (
+    <article ref={ref}>{postBody}</article>
+  ) : (
+    <article>{postBody}</article>
+  );
 
-                        return content
-  return (
-    <div> Post </div>
-  )
-})
+  return content;
+});
 
-export default Post
+export default Post;
