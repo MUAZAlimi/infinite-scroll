429ae6b51ff1dcfb2497018916397ac3d46e6eb7
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-22 10:45:12 +0100

    up

---
+++ src/hooks/usePosts.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import { getPostsPage } from "../api/axios";
+
+const usePosts = (pageNum = 1) => {
+    const [result, setResult] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState({});
+    const [isError, setIsError] = useState(false);
+    const [hasNextPage, setHasNextPage] = useState(false);
+
+    useEffect(() => {
+      setLoading(true)
+      setIsError(false)
+      setError([])
+
+      const controller = new AbortController();
+      const { signal } = controller
+
+      getPostsPage(pageNum, { signal })
+      .then(data => {
+        setResult(prev => [...prev, ...data])
+        setHasNextPage(Boolean(data.length))
+        setLoading(false)
+      })
+      .catch(e => {
+        setLoading(false)
+        if(signal.aborted) return
+        setIsError(true)
+        setError({ message: e.message})
+      })
+    }, [pageNum])
+    
+
+    return {loading, isError, error, result, hasNextPage}
+}
+
+export default usePosts
