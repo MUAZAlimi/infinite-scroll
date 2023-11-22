1a31c5f9b24f1c8520e7666c2f8304412ac5b41e
MUAZAlimi <112557869+MUAZAlimi@users.noreply.github.com>
2023-11-22 10:16:38 +0100

    up

--- src/axios.js
+++ src/axios.js
@@ -3,3 +3,8 @@ import axios from "axios";
 export const api = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com/'
 })
+
+export const getPostsPage = async(pageParam = 1, Options = {}) => {
+    const response = await api.get(`/posts?_page=${pageParam}`, Options)
+    return  response.data
+}
